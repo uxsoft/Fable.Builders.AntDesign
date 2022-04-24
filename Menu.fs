35ab3fd573f0fa5038902239cf799158d16249b7
Jan Dryk <jan.dryk@merck.com>
2022-04-24 19:07:47 +0200

    [BREAKING CHANGE] update menu to be compatible with antd 5 using json items instead of react element children.

--- Menu.fs
+++ Menu.fs
@@ -1,11 +1,11 @@
 namespace Fable.Builders.AntDesign
 
 open Browser.Types
+open Fable.Builders.AntDesign
 open Fable.Builders.Common
-open Fable.Import
 open Fable.Core
-open Fable.React
 open Fable.Core.JsInterop
+open Feliz
 
 [<AutoOpen>]
 module Menu =
@@ -15,6 +15,11 @@ module Menu =
         | Vertical 
         | Horizontal 
         | Inline
+    
+    [<StringEnum; RequireQualifiedAccess>]
+    type MenuTriggerSubMenuAction = 
+        | Hover 
+        | Click     
     
     type SelectParam = {
         key: string
@@ -31,8 +36,10 @@ module Menu =
         domEvent: DocumentEvent 
     }
     
+    type IMenuItem = interface end
+    
     type MenuBuilder() =
-        inherit ReactBuilder(import "Menu" "antd")
+        inherit ReactBuilder(null)
     
         [<CustomOperation("defaultOpenKeys")>] member inline _.defaultOpenKeys (x: DSLElement, v: string array) = x.attr "defaultOpenKeys" v 
         [<CustomOperation("defaultSelectedKeys")>] member inline _.defaultSelectedKeys (x: DSLElement, v: string array) = x.attr "defaultSelectedKeys" v 
@@ -49,38 +56,80 @@ module Menu =
         [<CustomOperation("subMenuCloseDelay")>] member inline _.subMenuCloseDelay (x: DSLElement, v: float) = x.attr "subMenuCloseDelay" v 
         [<CustomOperation("subMenuOpenDelay")>] member inline _.subMenuOpenDelay (x: DSLElement, v: float) = x.attr "subMenuOpenDelay" v 
         [<CustomOperation("theme")>] member inline _.theme (x: DSLElement, v: Theme) = x.attr "theme" v 
-        [<CustomOperation("triggerSubMenuAction")>] member inline _.triggerSubMenuAction (x: DSLElement, v: string) = x.attr "triggerSubMenuAction" v
+        [<CustomOperation("triggerSubMenuAction")>] member inline _.triggerSubMenuAction (x: DSLElement, v: MenuTriggerSubMenuAction) = x.attr "triggerSubMenuAction" v
         [<CustomOperation("onClick")>] member inline _.onClick (x: DSLElement, v: ClickParam -> unit) = x.attr "onClick" v 
         [<CustomOperation("onDeselect")>] member inline _.onDeselect (x: DSLElement, v: SelectParam -> unit) = x.attr "onDeselect" v 
         [<CustomOperation("onOpenChange")>] member inline _.onOpenChange (x: DSLElement, v: string array -> unit) = x.attr "onOpenChange" v 
         [<CustomOperation("onSelect")>] member inline _.onSelect (x: DSLElement, v: SelectParam -> unit) = x.attr "onSelect" v 
     
-    type TitleClickEvent = { key: string; domEvent: DocumentEvent }
+        member inline _.Yield (child: IMenuItem) =
+            { Attributes = []; Children = [ child |> box |> unbox<ReactElement> ] }
+        
+        member _.Run(s: DSLElement) =
+            let attrs = s.Attributes @ [
+                ("items", s.Children |> List.toArray |> box)
+            ]
+            Browser.Dom.console.log (createObj attrs)
+            Interop.reactApi.createElement(import "Menu" "antd", createObj attrs, [])
     
     type MenuItemBuilder() =
-        inherit ReactBuilder(import "Menu.Item" "antd")
+        inherit ReactBuilder(null)
     
         [<CustomOperation("danger")>] member inline _.danger (x: DSLElement, v: bool) = x.attr "danger" v 
         [<CustomOperation("disabled")>] member inline _.disabled (x: DSLElement, v: bool) = x.attr "disabled" v 
         [<CustomOperation("icon")>] member inline _.icon (x: DSLElement, v: ReactElement) = x.attr "icon" v
-        [<CustomOperation("title")>] member inline _.title (x: DSLElement, v: ReactElement) = x.attr "title" v 
-        [<CustomOperation("onClick")>] member inline _.onClick (x: DSLElement, v: unit -> unit) = x.attr "onClick" v
+        [<CustomOperation("title")>] member inline _.title (x: DSLElement, v: string) = x.attr "title" v 
+        [<CustomOperation("label")>] member inline _.label (x: DSLElement, v: ReactElement) = x.attr "label" v
+        
+        member inline _.Yield (child: IMenuItem) =
+            { Attributes = []; Children = [ child |> box |> unbox<ReactElement> ] }
+        
+        member _.Run (s: DSLElement) =
+            let attrs = s.Attributes
+            (createObj attrs) |> box |> unbox<IMenuItem>
     
     type MenuSubMenuBuilder() =
-        inherit ReactBuilder(import "Menu.SubMenu" "antd")
+        inherit ReactBuilder(null)
     
         [<CustomOperation("disabled")>] member inline _.disabled (x: DSLElement, v: bool) = x.attr "disabled" v
         [<CustomOperation("icon")>] member inline _.icon (x: DSLElement, v: ReactElement) = x.attr "icon" v
+        [<CustomOperation("label")>] member inline _.label (x: DSLElement, v: ReactElement) = x.attr "label" v
         [<CustomOperation("popupClassName")>] member inline _.popupClassName (x: DSLElement, v: string) = x.attr "popupClassName" v 
         [<CustomOperation("popupOffset")>] member inline _.popupOffset (x: DSLElement, v: float array) = x.attr "popupOffset" v
         [<CustomOperation("title")>] member inline _.title (x: DSLElement, v: ReactElement) = x.attr "title" v 
-        [<CustomOperation("onTitleClick")>] member inline _.onTitleClick (x: DSLElement, v: TitleClickEvent) = x.attr "onTitleClick" v 
+        [<CustomOperation("theme")>] member inline _.theme (x: DSLElement, v: Theme) = x.attr "theme" v 
+        [<CustomOperation("onTitleClick")>] member inline _.onTitleClick (x: DSLElement, v: {| key: string; domEvent: MouseEvent |} -> unit) = x.attr "onTitleClick" v 
+        
+        member inline _.Yield (child: IMenuItem) =
+            { Attributes = []; Children = [ child |> box |> unbox<ReactElement> ] }
+        
+        member _.Run (s: DSLElement) =
+            let attrs = s.Attributes @ [
+                ("children", box (s.Children |> List.toArray))
+            ]
+            (createObj attrs) |> box |> unbox<IMenuItem>
         
     type MenuItemGroupBuilder() =
-        inherit ReactBuilder(import "Menu.ItemGroup" "antd")
+        inherit ReactBuilder(null)
     
-        [<CustomOperation("title")>] member inline _.title (x: DSLElement, v: ReactElement) = x.attr "title" v
+        [<CustomOperation("label")>] member inline _.label (x: DSLElement, v: ReactElement) = x.attr "label" v
+        
+        member inline _.Yield (child: IMenuItem) =
+            { Attributes = []; Children = [ child |> box |> unbox<ReactElement> ] }
+        
+        member _.Run (s: DSLElement) =
+            let attrs = s.Attributes @ [
+                ("type", "group")
+                ("children", box (s.Children |> List.toArray))
+            ]
+            (createObj attrs) |> box |> unbox<IMenuItem>
         
     type MenuDividerBuilder() =
-        inherit ReactBuilder(import "Menu.Divider" "antd")
+        inherit ReactBuilder(null)
+        
+        [<CustomOperation("dashed")>] member inline _.dashed (x: DSLElement, v: bool) = x.attr "dashed" v 
+        
+        member _.Run (s: DSLElement) =
+            let attrs = s.Attributes @ [ ("type", box "divider") ]
+            (createObj attrs) |> box |> unbox<IMenuItem>
     
