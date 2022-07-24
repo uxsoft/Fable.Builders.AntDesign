7f082116ea2b4db17f3f8b719c67f89c5933e6c9
Jan Dryk <me@uxsoft.cz>
2022-07-24 22:51:55 +0200

    udpate typography builders

--- Typography.fs
+++ Typography.fs
@@ -20,36 +20,43 @@ module Typography =
         
         member x.Run(s: DSLElement) = { Name = "ellipsis";  Value = (createObj s.Attributes) }
     
-        [<CustomOperation("rows")>] member inline _.rows (x: DSLElement, v: int) = x.attr "rows" v
         [<CustomOperation("expandable")>] member inline _.expandable (x: DSLElement, v: bool) = x.attr "expandable" v
+        [<CustomOperation("rows")>] member inline _.rows (x: DSLElement, v: int) = x.attr "rows" v
         [<CustomOperation("suffix")>] member inline _.suffix (x: DSLElement, v: string) = x.attr "suffix" v
         [<CustomOperation("symbol")>] member inline _.symbol (x: DSLElement, v: ReactElement) = x.attr "symbol" v
-        [<CustomOperation("onExpand")>] member inline _.onExpand (x: DSLElement, v: Browser.Types.Event -> unit) = x.attr "onExpand" v
+        [<CustomOperation("tooltip")>] member inline _.tooltip (x: DSLElement, v: ReactElement) = x.attr "tooltip" v
         [<CustomOperation("onEllipsis")>] member inline _.onEllipsis (x: DSLElement, v: bool -> unit) = x.attr "onEllipsis" v
+        [<CustomOperation("onExpand")>] member inline _.onExpand (x: DSLElement, v: Browser.Types.Event -> unit) = x.attr "onExpand" v
+        
     
     type EditableConfigBuilder() =
         inherit ReactBuilder()
         
         member x.Run(s: DSLElement) = { Name = "editable";  Value = (createObj s.Attributes) }
         
-        [<CustomOperation("icon")>] member inline _.icon (x: DSLElement, v: ReactElement) = x.attr "icon" v
-        [<CustomOperation("tooltip")>] member inline _.tooltip (x: DSLElement, v: ReactElement) = x.attr "tooltip" v
-        [<CustomOperation("editing")>] member inline _.editing (x: DSLElement, v: bool) = x.attr "editing" v
-        [<CustomOperation("maxLength")>] member inline _.maxLength (x: DSLElement, v: int) = x.attr "maxLength" v
         [<CustomOperation("autoSize")>] member inline _.autoSize (x: DSLElement, v: bool) = x.attr "autoSize" v
+        [<CustomOperation("editing")>] member inline _.editing (x: DSLElement, v: bool) = x.attr "editing" v
+        [<CustomOperation("icon")>] member inline _.icon (x: DSLElement, v: ReactElement) = x.attr "icon" v
+        [<CustomOperation("maxLength")>] member inline _.maxLength (x: DSLElement, v: int) = x.attr "maxLength" v
+        [<CustomOperation("tooltip")>] member inline _.tooltip (x: DSLElement, v: ReactElement) = x.attr "tooltip" v
         [<CustomOperation("onStart")>] member inline _.onStart (x: DSLElement, v: unit -> unit) = x.attr "onStart" v
         [<CustomOperation("onChange")>] member inline _.onChange (x: DSLElement, v: string -> unit) = x.attr "onChange" v
+        [<CustomOperation("onCancel")>] member inline _.onCancel (x: DSLElement, v: unit -> unit) = x.attr "onCancel" v
+        [<CustomOperation("onEnd")>] member inline _.onEnd (x: DSLElement, v: unit -> unit) = x.attr "onEnd" v
+        [<CustomOperation("enterIcon")>] member inline _.enterIcon (x: DSLElement, v: ReactElement -> unit) = x.attr "enterIcon" v
+
     
     type CopyableConfigBuilder() =
         inherit ReactBuilder()
         
         member x.Run(s: DSLElement) = { Name = "copyable";  Value = (createObj s.Attributes) }
-    
-        [<CustomOperation("text")>] member inline _.text (x: DSLElement, v: string) = x.attr "text" v
-        [<CustomOperation("onCopy")>] member inline _.onCopy (x: DSLElement, v: unit -> unit) = x.attr "onCopy" v
+        
+        [<CustomOperation("format")>] member inline _.format (x: DSLElement, v: string) = x.attr "format" v
         [<CustomOperation("icon")>] member inline _.icon (x: DSLElement, v: ReactElement) = x.attr "icon" v
+        [<CustomOperation("text")>] member inline _.text (x: DSLElement, v: string) = x.attr "text" v
         [<CustomOperation("tooltips")>] member inline _.tooltips (x: DSLElement, v: bool) = x.attr "tooltips" v
-    
+        [<CustomOperation("onCopy")>] member inline _.onCopy (x: DSLElement, v: unit -> unit) = x.attr "onCopy" v
+        
     type TypographyBuilder() =
         inherit ReactBuilder()
         
